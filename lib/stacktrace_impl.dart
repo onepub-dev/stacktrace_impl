34261aa2ec534522ac06c0cdec584ed04d9840a9
Brett Sutton <bsutton@noojee.com.au>
2022-06-09 12:29:46 +1000

    add copyright notices.

--- lib/stacktrace_impl.dart
+++ lib/stacktrace_impl.dart
@@ -1,2 +1,8 @@
+/* Copyright (C) S. Brett Sutton - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
+
 export 'src/stackframe.dart';
 export 'src/stacktrace_impl.dart';
